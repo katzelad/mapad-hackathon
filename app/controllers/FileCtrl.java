0e1c2d6224c6cb801b9508465ebd4aaa85e96651
ronenRubinov <ronen5353@gmail.com>
2016-09-28 14:27:28 +0300

    add new file java

--- app/controllers/FileCtrl.java
+++ app/controllers/FileCtrl.java
@@ -2,13 +2,15 @@ package controllers;
 
 import java.io.File;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import play.*;
 import play.mvc.*;
 
 public class FileCtrl extends Controller {
 
 	public static Result getFileUpload() {
-		String fileObject = request().body().asText();
+		JsonNode fileObject = request().body().asJson();
 		File file = request().body().asRaw().asFile();
 		
 		System.out.println(file.toString() + "" + fileObject.toString());
