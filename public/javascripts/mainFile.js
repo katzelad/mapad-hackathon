4d8f5894d425cac1d4a4fce67a561229f1a43682
orsagiv <orsagiv1994@gmail.com>
2016-09-28 14:28:15 +0300

    or

--- public/javascripts/mainFile.js
+++ public/javascripts/mainFile.js
@@ -1,10 +1,29 @@
-  var FileApp = angular.module("FileApp", []);
+  var FileApp = angular.module("FileApp", ['ngFileUpload']);
 
 
-  FileApp.controller("FileController", function($scope) {
+  FileApp.controller("FileController", function($scope, Upload, $timeout) {
 
-      $scope.fileChange = function(fileValue) {
+    $scope.fileChange = function(fileValue) {
           console.log(fileValue);
       }
+      
+    $scope.uploadPic = function(file) {
+        file.upload = Upload.upload({
+          url: 'C:\GitProjects\mapad-hackathon\files\orYoung.pptx',
+          data: {username: $scope.username, file: file},
+        });
+
+        file.upload.then(function (response) {
+          $timeout(function () {
+            file.result = response.data;
+          });
+        }, function (response) {
+          if (response.status > 0)
+            $scope.errorMsg = response.status + ': ' + response.data;
+        }, function (evt) {
+          // Math.min is to fix IE which reports 200% sometimes
+          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        });
+        }
 
   })
