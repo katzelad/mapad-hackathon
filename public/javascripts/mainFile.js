f905af2e312a748c0581621052db3fff0913cdb6
ronenRubinov <ronen5353@gmail.com>
2016-09-28 19:20:46 +0300

    all script code in to one page controller

--- public/javascripts/mainFile.js
+++ public/javascripts/mainFile.js
@@ -1,10 +1,93 @@
-  var FileApp = angular.module("FileApp", []);
+      function initAutocomplete() {
+          var map = new google.maps.Map(document.getElementById('map'), {
+              center: {
 
+                  lat: 31.771959,
+                  lng: 35.217018
+              },
+              zoom: 7,
+              mapTypeId: 'roadmap'
+          });
 
-  FileApp.controller("FileController", function($scope) {
+          // Create the search box and link it to the UI element.
+          var input = document.getElementById('pac-input');
+          var searchBox = new google.maps.places.SearchBox(input);
+          map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-      $scope.fileChange = function(fileValue) {
-          console.log(fileValue);
+          // Bias the SearchBox results towards current map's viewport.
+          map.addListener('bounds_changed', function() {
+              searchBox.setBounds(map.getBounds());
+          });
+
+          var markers = [];
+          // Listen for the event fired when the user selects a prediction and retrieve
+          // more details for that place.
+          searchBox.addListener('places_changed', function() {
+              var places = searchBox.getPlaces();
+
+              if (places.length == 0) {
+                  return;
+              }
+
+              // Clear out the old markers.
+              markers.forEach(function(marker) {
+                  marker.setMap(null);
+              });
+              markers = [];
+
+              // For each place, get the icon, name and location.
+              var bounds = new google.maps.LatLngBounds();
+              places.forEach(function(place) {
+                  if (!place.geometry) {
+                      console.log("Returned place contains no geometry");
+                      return;
+                  }
+                  var icon = {
+                      url: place.icon,
+                      size: new google.maps.Size(71, 71),
+                      origin: new google.maps.Point(0, 0),
+                      anchor: new google.maps.Point(17, 34),
+                      scaledSize: new google.maps.Size(25, 25)
+                  };
+
+                  // Create a marker for each place.
+                  markers.push(new google.maps.Marker({
+                      map: map,
+                      icon: icon,
+                      title: place.name,
+                      position: place.geometry.location
+                  }));
+
+                  if (place.geometry.viewport) {
+                      // Only geocodes have viewport.
+                      bounds.union(place.geometry.viewport);
+                  } else {
+                      bounds.extend(place.geometry.location);
+                  }
+              });
+              map.fitBounds(bounds);
+          });
       }
+       //-----------------------------------------------------------------------------------------
+      var FileApp = angular.module("FileApp", ["ngFileUpload"]);
 
-  })
+
+
+      FileApp.controller('fileUploadCtrl', ['$scope', 'Upload', '$timeout', '$http',
+          function($scope, Upload, $timeout, $http) {
+              $scope.uploadPic = function(file) {
+                  var fd = new FormData();
+                  fd.append('file', file);
+
+                  $http.post("/clientFile/upload", fd, {
+                      transformRequest: angular.identity,
+                      headers: {
+                          'Content-Type': undefined
+                      }
+                  }).error(function(err) {
+                      console.log("error");
+                  })
+
+              }
+          }
+      ]);
